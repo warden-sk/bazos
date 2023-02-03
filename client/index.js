64489cdc3a92e9d02db6c5311607b84d4ab79449
Marek Kobida <marek.kobida@gmail.com>
2023-02-03 05:16:38 +0100

    popisy

--- client/index.js
+++ client/index.js
@@ -5,6 +5,11 @@
 const getProductsElement = window.document.querySelector('#getProducts');
 const tableElement = window.document.querySelector('#table');
 
+/**
+ * Do tabuľky vytvorí riadok so stĺpcami
+ *
+ * columns: (Node | string)[]
+ */
 function createTableRow(columns) {
   const newTableRow = tableElement.insertRow();
 
@@ -15,16 +20,28 @@ function createTableRow(columns) {
   return newTableRow;
 }
 
+/**
+ * Po kliknutí na „Get products“
+ */
 getProductsElement.addEventListener('click', async () => {
   const response = await fetch('https://raw.githubusercontent.com/warden-sk/bazos/main/json/products.json');
 
+  /**
+   * json: Product[]
+   */
   const json = await response.json();
 
+  /**
+   * Prvý riadok so stĺpcami
+   */
   createTableRow(['address', 'name', 'price']);
 
   json.forEach(product => {
     const newTableRow = createTableRow([product.address[0], product.name, `${product.price} €`]);
 
+    /**
+     * Po kliknutí na riadok
+     */
     newTableRow.addEventListener('click', () => {
       window.open(`https://elektro.bazos.sk/inzerat/${product.id}/.php`, '_blank');
     });

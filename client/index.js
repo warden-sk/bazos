3b61a45153f186a4fd40218ca54727318f22cd29
Marek Kobida <marek.kobida@gmail.com>
2023-02-04 13:08:08 +0100

    updated client

--- client/index.js
+++ client/index.js
@@ -3,15 +3,17 @@
  */
 
 const getProductsElement = window.document.querySelector('#getProducts');
-const tableElement = window.document.querySelector('#table');
+
+const updatesElement = window.document.querySelector('#updates');
+const productsElement = window.document.querySelector('#products');
 
 /**
  * Do tabuľky vytvorí riadok so stĺpcami
  *
  * columns: (Node | string)[]
  */
-function createTableRow(columns) {
-  const newTableRow = tableElement.insertRow();
+function createTableRow(table, columns) {
+  const newTableRow = table.insertRow();
 
   columns.forEach(column => {
     newTableRow.insertCell().append(column);
@@ -25,21 +27,28 @@ function createTableRow(columns) {
  */
 getProductsElement.addEventListener('click', async () => {
   const response1 = await fetch('https://raw.githubusercontent.com/warden-sk/bazos/main/json/products.json');
+  const response2 = await fetch('https://raw.githubusercontent.com/warden-sk/bazos/main/json/updates.json');
 
   /**
    * productStorage: Product[]
    */
   let productStorage = await response1.json();
 
+  /**
+   * updateStorage: Update[]
+   */
+  const updateStorage = await response2.json();
+
   productStorage = productStorage.sort((l, r) => r.date - l.date);
 
   /**
    * Prvý riadok so stĺpcami
    */
-  createTableRow(['address', 'date', 'name', 'price']);
+  createTableRow(productsElement, ['address', 'date', 'name', 'price']);
+  createTableRow(updatesElement, ['column', 'from', 'to']);
 
   productStorage.forEach(product => {
-    const newTableRow = createTableRow([
+    const newTableRow = createTableRow(productsElement, [
       product.address[0],
       new Date(product.date).toLocaleDateString(),
       product.name,
@@ -56,5 +65,9 @@ getProductsElement.addEventListener('click', async () => {
     } else {
       newTableRow.classList.add('isNotActive');
     }
+  });
+
+  updateStorage.forEach(update => {
+    createTableRow(updatesElement, [update.column, update.from, update.to]);
   });
 });
